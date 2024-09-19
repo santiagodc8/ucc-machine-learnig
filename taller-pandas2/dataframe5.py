770029c23566cc56f563eba1ba20c0e0ae9aa86c
Santiago <santidurancanedo@gmail.com>
2024-09-19 11:00:29 -0500

    region INLAND

--- taller-pandas2/dataframe5.py
+++ taller-pandas2/dataframe5.py
@@ -7,3 +7,8 @@ datos = pd.read_csv("/home/santiagod/Documentos/ucc-repos/ucc-machine-learnig/ta
 print("\nvalores nulos por columna: ")
 print(datos.isnull().sum())
 
+# Buscar todas las casas en la region de "INLAND" con mas de 4 habitaciones
+casas_inland = datos[(datos["ocean_proximity"] == "INLAND") & (datos["total_rooms"] > 4000)]
+print("\ncasas en la region INLAND con mas de 4000 habitaciones: ")
+print(casas_inland)
+casas_inland.to_csv("inmoviliaria_inland.csv", index = False)
