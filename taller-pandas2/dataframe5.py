d79df5608022d58b332f805f7f47cb7c64e371e8
Santiago <santidurancanedo@gmail.com>
2024-09-19 11:06:53 -0500

    casas mayor a 500.000

--- taller-pandas2/dataframe5.py
+++ taller-pandas2/dataframe5.py
@@ -12,3 +12,9 @@ casas_inland = datos[(datos["ocean_proximity"] == "INLAND") & (datos["total_room
 print("\ncasas en la region INLAND con mas de 4000 habitaciones: ")
 print(casas_inland)
 casas_inland.to_csv("inmoviliaria_inland.csv", index = False)
+
+# Recorrer el dataframe y mostrar la region y valor de la casa si es mayor a 500.000
+print('\nMostrar la región y valor de la casa si es mayor 500.000')
+for index, row in datos.iterrows():
+  if row['median_house_value'] > 500000:
+    print(f"Región: {row['ocean_proximity']}, Valor: {row['median_house_value']}")
